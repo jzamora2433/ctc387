25b3642ed64b9ee1c192ed1a54cd559d787f1540
jzamora2433 <jzamora2433@gmail.com>
2026-03-19 01:12:27 +0000

    midterm question 14

---
+++ zamoramidterm.py
@@ -0,0 +1,12 @@
+#Josue Zamora
+#387 Midterm
+
+x= 8
+number= int(input("please choose a number between 1 and 10: "))
+
+if number == 8:
+    print("G8!!! You guessed correctly!")
+
+else:
+    print("you did not guess correctly", number, "is incorrect")
+
